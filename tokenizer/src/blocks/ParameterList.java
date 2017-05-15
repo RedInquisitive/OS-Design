f468470da7e875298e18b5f5b1729cd314eb2364
AJ Walter <aj.walter@aol.com>
2017-05-15 18:13:15 -0500

    Expressions and Parameter Lists

---
+++ tokenizer/src/blocks/ParameterList.java
@@ -0,0 +1,47 @@
+package blocks;
+
+import java.text.ParseException;
+
+import org.w3c.dom.Element;
+
+import io.Reader;
+import io.Token;
+import structure.Lexical;
+import structure.Program;
+import symbols.Symbol;
+import tokenizer.Base;
+import tokenizer.Main;
+
+public class ParameterList extends Base {
+
+	protected ParameterList(Element root) {
+		super(root);
+	}
+
+	/**
+	 * Reads tokens until either invalid syntax (exception) or
+	 * missing comma. In the case of the missing comma, the reader
+	 * will abort.
+	 */
+	public void run(Token header) throws ParseException {
+		Main.read.abort();
+
+		while(true) {
+			Token next = Main.read.next();
+			if(!Type.verify(next)) 
+				break;
+			new Type(root).run(next);
+			
+			next = Main.read.next();
+			if(next.getLexical() != Lexical.IDENTIFIER)
+				throw new ParseException("Expected a variable name in parameter list!", Reader.getCount());
+			append(next, Program.VAR_NAME);
+			
+			next = Main.read.next();
+			if(next.getSymbol() != Symbol.COMMA)
+				break;
+			append(next);
+		}
+		Main.read.abort();
+	}
+}
---
+++ tokenizer/src/structure/Express.java
@@ -0,0 +1,25 @@
+package structure;
+
+import java.text.ParseException;
+
+import tokenizer.Main;
+import xml.XmlName;
+
+public enum Express implements XmlName {
+	EXPRESSION_LIST("expressionList"),
+	EXPRESSION("expression"),
+	TERM("term");
+	
+	private final String name;
+	private Express(String name) {
+		this.name= name;
+	}
+
+	public String xml() {
+		return name;
+	}
+	
+	public String xmlText() throws ParseException {
+		throw Main.textlessXML(getClass().getName());
+	}
+}
