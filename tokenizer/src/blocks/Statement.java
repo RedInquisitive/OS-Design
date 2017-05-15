82d4a5e7f375abeece1074a4569aa63efa90cbd6
Aaron Walter <aj.walter@aol.com>
2017-05-15 14:34:35 -0500

    Fix

--- tokenizer/src/blocks/Statement.java
+++ tokenizer/src/blocks/Statement.java
@@ -27,27 +27,27 @@ public class Statement extends Base {
 		
 		next = Main.read.next();
 		Element statement = null;
-		if(!LetStatement.verify(next)) {
+		if(LetStatement.verify(next)) {
 			statement = decend(Statements.LET_STATEMENT);
 			new LetStatement(statement).run(next);
 			return;
 		}
-		if(!IfStatement.verify(next)) {
+		if(IfStatement.verify(next)) {
 			statement = decend(Statements.IF_STATEMENT);
 			new IfStatement(statement).run(next);
 			return;
 		}
-		if(!WhileStatement.verify(next)) {
+		if(WhileStatement.verify(next)) {
 			statement = decend(Statements.WHILE_STATEMENT);
 			new WhileStatement(statement).run(next);
 			return;
 		}
-		if(!DoStatement.verify(next)) {
+		if(DoStatement.verify(next)) {
 			statement = decend(Statements.DO_STATEMENT);
 			new DoStatement(statement).run(next);
 			return;
 		}
-		if(!ReturnStatement.verify(next)) {
+		if(ReturnStatement.verify(next)) {
 			statement = decend(Statements.RETURN_STATEMENT);
 			new ReturnStatement(statement).run(next);
 			return;
